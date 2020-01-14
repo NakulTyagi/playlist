af71c5403fa31dad71d37aa37af16a6d7a0d6be6
Nakul Tyagi <31548192+NakulTyagi@users.noreply.github.com>
2020-01-15 01:14:44 +0530

    Updates

---
+++ precache-manifest.c8dda56d1c9e29c609c450a17d9d5aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a32b5f38fe64b0566c48b815b1f788",
+    "url": "/playlist/index.html"
+  },
+  {
+    "revision": "7bbe8ef9f49874378d53",
+    "url": "/playlist/static/css/main.7642a6e9.chunk.css"
+  },
+  {
+    "revision": "76f4a5a362384427f4d9",
+    "url": "/playlist/static/js/2.582fda6e.chunk.js"
+  },
+  {
+    "revision": "fe54a4113474ace11aa103e68819ed08",
+    "url": "/playlist/static/js/2.582fda6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bbe8ef9f49874378d53",
+    "url": "/playlist/static/js/main.d03c470c.chunk.js"
+  },
+  {
+    "revision": "8ee17abfdd35acdf6b51",
+    "url": "/playlist/static/js/runtime-main.dd35ccfb.js"
+  }
+]);
